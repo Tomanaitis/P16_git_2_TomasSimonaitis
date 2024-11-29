eeac1dee9a66c2ed42a27a0f64a85691f99b6624
Burnok420 <boombox1x@gmail.com>
2024-11-29 08:56:36 +0200

    Įdėti pirmi 2 Python failai
    1.darbuotojai
    2.kaupk_daugyba

---
+++ Darbuotojai.py
@@ -0,0 +1,24 @@
+darbuotojas_1 = ["Adomas", "direktorius", 2500]
+darbuotojas_2 = ["Romas", "direktorius", 2000]
+darbuotojas_3 = ["Valdas", "vadybininkas", 3000]
+# tikrinam kodo teisingumą keisdami po 1 darbuotojus
+darbuotojas = darbuotojas_1
+
+if (darbuotojas[1] == "direktorius") and (darbuotojas[2] > 2000):
+    print(f"{darbuotojas} darbuotojas yra direktorius ir uždirba daugiau nei 2000")
+else:
+    print(f"darbuotojas nera direktorius arba uždirba mažiau nei 2000")
+
+darbuotojas = darbuotojas_2
+
+if (darbuotojas[1] == "direktorius") and (darbuotojas[2] > 2000):
+    print(f"{darbuotojas} darbuotojas yra direktorius ir uždirba daugiau nei 2000")
+else:
+    print(f"darbuotojas nera direktorius arba uždirba mažiau nei 2000")
+
+darbuotojas = darbuotojas_3
+
+if (darbuotojas[1] == "direktorius") and (darbuotojas[2] > 2000):
+    print(f"{darbuotojas} darbuotojas yra direktorius ir uždirba daugiau nei 2000")
+else:
+    print(f"darbuotojas nera direktorius arba uždirba mažiau nei 2000")
---
+++ kaupk_daugyba.py
@@ -0,0 +1,18 @@
+def kaupk_daugyba(*args):
+    if len(args) == 0:
+        return 0
+    kaupiklis = args[0]
+    for elem in args[1:]:
+        kaupiklis *= elem
+    return kaupiklis
+
+
+res = kaupk_daugyba(1)  # 1
+print(res)
+res = kaupk_daugyba(2, 2)  # 4
+print(res)
+res = kaupk_daugyba(2, 2, 2)  # 8
+print(res)
+res = kaupk_daugyba(0, 2, 2)  # 0
+print(res)
+
